dac560dfe61fd019f3593809bb3042dfd79c4cc4
likith250199 <likith250199@gmail.com>
2026-03-16 15:22:53 +0530

    likith

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps from Linux!"
+    return "Hello DevOps this is my first project"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
